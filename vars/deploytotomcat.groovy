0c3122ab5cde7f5b4aa7fd48aa63adc038603c9d
Augustine Ozor <99667583+AustinOzor@users.noreply.github.com>
2024-06-07 23:57:54 +0300

    Update deploytotomcat.groovy

--- vars/deploytotomcat.groovy
+++ vars/deploytotomcat.groovy
@@ -1,3 +1,1 @@
-def call () {
-    deploy adapters: [tomcat9(credentialsId: 'tomcat-cred', path: '', url: 'http://54.146.92.27:8080/')], contextPath: 'Planview', onFailure: false, war: 'target/*.war'   
-}
+
